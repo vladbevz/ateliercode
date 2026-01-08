f69cc6089804fada17e9279fda21ee5929252a2f
vbevz <122693446+vladbevz@users.noreply.github.com>
2026-01-08 17:11:50 +0100

    Fix ease array in Tarifs component

--- app/components/Tarifs.tsx
+++ app/components/Tarifs.tsx
@@ -92,7 +92,7 @@ export default function Tarifs() {
       opacity: 1,
       transition: { 
         duration: 0.6,
-        ease: [0.22, 1, 0.36, 1]
+        ease: [0.22, 1, 0.36, 1] as const
       }
     }
   };
